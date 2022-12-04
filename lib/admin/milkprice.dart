8b1779489661b819ccad0111486999ee2a6848d2
Pavan Bhadane <pavan49719@gmail.com>
2022-12-04 19:51:32 +0530

    Added

---
+++ lib/admin/milkprice.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MilkPrice extends StatefulWidget {
+  const MilkPrice({Key? key}) : super(key: key);
+
+  @override
+  State<MilkPrice> createState() => _MilkPriceState();
+}
+
+class _MilkPriceState extends State<MilkPrice> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/admin_dashboard.dart
+++ lib/admin_dashboard.dart
@@ -10,6 +10,75 @@ class AdminDashboard extends StatefulWidget {
 class _AdminDashboardState extends State<AdminDashboard> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Admin\'s Dashboard'),
+      ),
+      body: Column(children: [
+        Row(
+          children: [
+            ElevatedButton(
+      child: Container(
+        child: Column(
+          children: [
+            Icon(Icons.grass),
+            Text('Farmer\'s detail'),
+          ],
+        ),
+      ),
+      onPressed: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context)=>))
+      },
+    ),
+    ElevatedButton(
+      child: Container(
+        child: Column(
+          children: [
+            Icon(Icons.checklist_rtl),
+            Text('Orders'),
+          ],
+        ),
+      ),
+      onPressed: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context)=>))
+      },
+    ),
+          ],
+        ),
+        Row(
+          children: [
+            ElevatedButton(
+      child: Container(
+        child: Column(
+          children: [
+            Icon(Icons.plus_one),
+            Text('Add Milk'),
+          ],
+        ),
+      ),
+      onPressed: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context)=>))
+      },
+    ),
+    ElevatedButton(
+      child: Container(
+        child: Column(
+          children: [
+            Icon(Icons.price_change),
+            Text('Set Milk Price'),
+          ],
+        ),
+      ),
+      onPressed: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context)=>))
+      },
+    ),
+    
+          ],
+        )
+      ]),
+    );
   }
 }
+
+
