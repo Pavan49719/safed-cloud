b27a77db99954e55f52b28239aaf48f86c06f651
Pavan Bhadane <pavan49719@gmail.com>
2022-12-06 20:49:45 +0530

    added

--- lib/register.dart
+++ lib/register.dart
@@ -45,6 +45,11 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
       final json = {
         'email': email.text.trim(),
         'name': name.text.trim(),
+        'aadhar':'',
+        'acc-no':'',
+        'age':'',
+        'ifsc-code':'',
+        'location':'',
       };
 
       await docUser.set(json);
@@ -60,11 +65,6 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
       final json = {
         'email': email.text.trim(),
         'name': name.text.trim(),
-        'aadhar':'',
-        'acc-no':'',
-        'age':'',
-        'ifsc-code':'',
-        'location':'',
       };
 
       await docUser.set(json);
