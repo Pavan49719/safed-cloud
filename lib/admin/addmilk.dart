e3a1cfa526a88fce7f3e4a2b63f5c47992cd8419
Maruti54 <patilmaruti9604@gmail.com>
2022-12-09 17:44:21 +0530

    Updated

--- lib/admin/addmilk.dart
+++ lib/admin/addmilk.dart
@@ -1,15 +1,108 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class AddMilk extends StatefulWidget {
-  const AddMilk({Key? key}) : super(key: key);
+class Scanner extends StatefulWidget {
+  const Scanner({Key? key}) : super(key: key);
 
   @override
-  State<AddMilk> createState() => _AddMilkState();
+  _ScannerState createState() => _ScannerState();
 }
 
-class _AddMilkState extends State<AddMilk> {
+class _ScannerState extends State<Scanner> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  late QRViewController controller;
+
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) async {
+      controller.pauseCamera();
+      if (await canLaunchUrl(Uri.parse((scanData.code)!))) {
+        await launchUrl(Uri.parse((scanData.code)!));
+        controller.resumeCamera();
+      } else {
+        showDialog(
+          context: this.context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Url'),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: <Widget>[
+                    Text('Barcode Type: ${describeEnum(scanData.format)}'),
+                    Text('Data: ${scanData.code}'),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('Ok'),
+                  onPressed: () {
+
+                  },
+                ),
+              ],
+            );
+          },
+        ).then((value) => controller.resumeCamera());
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        QRView(
+          key: qrKey,
+          onQRViewCreated: _onQRViewCreated,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Container(
+                width: 300,
+                height: 300,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.red,
+                    width: 4,
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 100,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+
+}
+
+Future<void> _launchInBrowser(Uri url) async {
+  if (!await launchUrl(
+    url,
+    mode: LaunchMode.externalApplication,
+  )) {
+    throw 'Could not launch $url';
   }
 }
+
+void _showErrorMes(String mes, BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(mes)));
+}
+
